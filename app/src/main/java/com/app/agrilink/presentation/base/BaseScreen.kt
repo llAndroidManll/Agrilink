4dda317f8560d4ac1efde6bfe7da29fe645133c9
llAndroidManll <sahakyansahak404@gmail.com>
2024-12-07 21:04:00 +0400

    Changing BaseScreen

--- app/src/main/java/com/app/agrilink/presentation/base/BaseScreen.kt
+++ app/src/main/java/com/app/agrilink/presentation/base/BaseScreen.kt
@@ -1,6 +1,5 @@
 package com.app.agrilink.presentation.base
 
-import BaseViewModel
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
